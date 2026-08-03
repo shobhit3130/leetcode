7e213d01ed9dca5788168a51097ae6847c46dc7d
shobhit3130 <shobhitg3130@gmail.com>
2026-08-03 23:25:02 +0530

    Time: 2 ms (95.54%), Space: 48.4 MB (61.79%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -3,21 +3,18 @@ class Solution {
         int i=0;
         int j=numbers.length-1;
         while(i<j){
-        int sum=numbers[i]+numbers[j];
-        if(sum==target){
-            return new int[]{i+1,j+1};
+            int sum=numbers[i]+numbers[j];
+            if(sum==target){
+                return new int[]{i+1,j+1};
+            }
+            else if(sum>target){
+                j--;
+            }
+            else{
+                i++;
+            }
         }
-
-        else if(sum>target){
-            j--;
-        }
-        else if(target>sum){
-            i++;
-        }
-        
-        }
-        return new int[]{i+1,j+1};
-        
+        return new int[]{};
         
     }
 }
